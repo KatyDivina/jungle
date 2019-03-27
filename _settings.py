c91c060f5dcadea403ce05cf9d05f579fc0a84d5
KatyDivina <ekaterinadivina@gmail.com>
2019-03-27 23:11:18 +0300

    Refactor background, add convert_alpha

--- _settings.py
+++ _settings.py
@@ -5,19 +5,22 @@ clock = pygame.time.Clock()
 pygame.time.set_timer(pygame.USEREVENT,50)
 
 #загрузка фонов
-bg = []
+bg= []
 
 bg.append(pygame.image.load("background/plx-1.png"))
 bg.append(pygame.image.load("background/plx-2.png"))
 bg.append(pygame.image.load("background/plx-3.png"))
 bg.append(pygame.image.load("background/plx-4.png"))
 bg.append(pygame.image.load("background/plx-5.png"))
-scale = 2
-for i in range(len(bg)):
-    bg[i] = pygame.transform.scale(bg[i],(bg[i].get_width()*scale,bg[i].get_height()*scale))
-HIGH=bg[1].get_height()
-SIZE=bg[1].get_width()
-gamedisplay = pygame.display.set_mode((SIZE, HIGH))
-pygame.display.set_caption("Jungl")
-grav = 5
 
+
+SCALE = 3
+HIGH= bg[1].get_height()  * SCALE
+SIZE= bg [1]  .get_width() * SCALE
+
+gamedisplay = pygame.display.set_mode((SIZE, HIGH))
+pygame.display.set_caption("Jungle")
+
+
+for i in range(len(bg)):
+    bg[i] = pygame.transform.scale(bg[i].convert_alpha(), (SIZE, HIGH))
